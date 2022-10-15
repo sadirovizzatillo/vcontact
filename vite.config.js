62c6a91088dcf18b4d25e39a50421193c0bf2a64
sadirov4647 <sadirovizzatillo3@gmail.com>
2022-10-15 11:11:54 +0500

    v1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'development'
-  ? '/my/dev/baseurl/'
-  : '/my/prod/baseurl/',
+  // base: process.env.NODE_ENV === 'development'
+  // ? '/my/dev/baseurl/'
+  // : '/my/prod/baseurl/',
+  // resolve: {
+  //   alias: {
+  //     "~": path.resolve(__dirname, "node_modules"),
+  //     "@": path.resolve(__dirname, "src"),
+  //   },
+  // },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+
 })
